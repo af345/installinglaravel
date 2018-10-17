b7610c3b5d5a85f00745539929f6200faf8511fa
alexandraflores <af345@njit.edu>
2018-10-16 22:50:34 -0400

    task: added content to my home and about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,4 +3,13 @@
 
     <h1>Hello World Welcome to my About Page</h1>
 
+    <br>
+
+    <br>
+
+    <br>
+
+    <b> This is my About Page </b>
+    <p> By Alexandra Flores </p>
+
 @stop
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,4 +3,13 @@
 
     <h1>Hello World Welcome to my Home Page</h1>
 
+    <br>
+
+    <br>
+
+    <br>
+
+    <b> This is my Home Page </b>
+    <p> By Alexandra Flores </p>
+
 @stop
