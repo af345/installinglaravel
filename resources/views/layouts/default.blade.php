ae1c949393a94ca867f8696260d028a441f68445
alexandraflores <af345@njit.edu>
2018-10-16 21:21:14 -0400

    task: added default.blade.php to layouts directories

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('includes.head')
+</head>
+<body>
+
+<header>
+    @include('includes.header')
+</header>
+
+<main role="main">
+
+        @yield('content')
+        <footer class="row">
+            @include('includes.header')
+        </footer>
+
+</main>
+<script
+
+<script
+
+<script
+
+</body>
+</html>
